62bae2de00ca82bf050d73c78623c1492c58be49
mauerv <maurovelazquez1@gmail.com>
2019-06-03 20:35:42 -0300

    Remove file

--- build/static/js/main.3b2b4673.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[,,,function(n,e,t){n.exports=t(11)},,,,,,function(n,e,t){},function(n,e,t){},function(n,e,t){"use strict";t.r(e);var a=t(0),c=t.n(a),o=t(2),r=t.n(o);t(9),t(10);var u=function(){return c.a.createElement("div",{className:"App"},c.a.createElement("p",null,"One Big Lab"))};r.a.render(c.a.createElement(u,null),document.getElementById("root"))}],[[3,1,2]]]);
-//# sourceMappingURL=main.3b2b4673.chunk.js.map
