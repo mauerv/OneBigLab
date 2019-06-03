d68c4dca2b4b46c27eda539f3f2af6cbada5750b
mauerv <maurovelazquez1@gmail.com>
2019-06-03 20:01:38 -0300

    Initial commit

---
+++ build/precache-manifest.4cf369748df43bdbbb3ebb527efe680f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f46ff749659626f04328bee415def0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d66dfdb2485fff333025",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "d66dfdb2485fff333025",
+    "url": "/static/js/main.3b2b4673.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
