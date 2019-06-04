f5abfdad98ddc87c04e06edde1fe72eca569d1fa
mauerv <maurovelazquez1@gmail.com>
2019-06-03 22:35:52 -0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-        <nav className="navbar navbar-dark bg-primary">
-				  <a className="navbar-brand" href="#">OneBigLab</a>
+    <div className='App'>
+        <nav className='navbar navbar-dark bg-primary'>
+				  <a className='navbar-brand' href='#'>OneBigLab</a>
 				</nav>
+
+				<div className='fixed-bottom bg-primary p-4 d-flex'>
+					<p className='text-light m-0 align-self-center'>DAO Directory by <strong><a className='text-light' href='https://twitter.com/emivelazquez6' target='_blank'>emi</a></strong> and <strong><a className='text-light' href='https://twitter.com/MauroVelazquezz' target='_blank'>mauerv</a></strong>
+					</p>
+				</div>
     </div>
   );
 }
