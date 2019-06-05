2bbbebba021f5990de30e493081667d85df6f06b
mauerv <maurovelazquez1@gmail.com>
2019-06-05 17:46:33 -0300

    Replace components for Material UI

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 import Hero from './Hero'
 import Feature from './Feature'
@@ -16,36 +17,45 @@ export default () => (
 			buttonText='Explore DAOs'
 			buttonLink={ROUTES.DAOS}
 		/>
-		<div className='container'>
-			<div className='border-top border-bottom pt-4 pb-3'>
-				<h3>What is a DAO</h3>
-				<p>
-					A decentralized autonomous organization (DAO), sometimes labeled a decentralized autonomous 
-					corporation (DAC), is an organization represented by rules encoded as a computer program that is transparent, 
-					controlled by shareholders and not influenced by a central government. 
-					A DAO's financial transaction record and program rules are maintained on a blockchain.
-				</p>
-			</div>
-			<div className='border-top border-bottom pt-4 pb-3'>
-				<h3>How to start one?</h3>
-				<p>These days there are multiple frameworks, platforms or code you can fork in order to start your own DAO. It can have its own blockchain, or live as a smart contract on top of a platform like <a href='https://ethereum.org/' target='_blank'>Ethereum</a> or <a href='https://www.rsk.co/' target='_blank'>Rootstock</a>.</p>
-				<Link to={ROUTES.CREATE_INFO} className='btn btn-primary'>Start a DAO</Link>
-			</div>
-			<div className='border-top border-bottom pt-4 pb-3'>
-				<h3>Featured DAOs</h3>
-				<div className='row d-flex flex-wrap'>
-					{daos.map(dao => (
-						<div className='col-6 col-md-3 text-center'>
-							<div className='grow mb-3'>
-								<Link to={`${ROUTES.DAOS}/${dao.id}`} className=' mr-1'>
-									<img src={dao.image} className='img-thumbnail' />
-								</Link>
-							</div>
-							<p className='font-weight-bold'>{dao.title}</p>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
+
+		<MDBContainer>
+			<MDBRow className='border-top border-bottom pt-4 pb-4'>
+				<MDBCol className='col-12'>
+					<h3>What is a DAO</h3>
+				</MDBCol>
+				<MDBCol className='col-12'>
+					<p>
+						A decentralized autonomous organization (DAO), sometimes labeled a decentralized autonomous 
+						corporation (DAC), is an organization represented by rules encoded as a computer program that is transparent, 
+						controlled by shareholders and not influenced by a central government. 
+						A DAO's financial transaction record and program rules are maintained on a blockchain.
+					</p>
+				</MDBCol>
+			</MDBRow>
+			<MDBRow className='border-top border-bottom pt-4 pb-4'>
+				<MDBCol className='col-12'><h3>How to start one?</h3></MDBCol>
+				<MDBCol className='col-12'><p>These days there are multiple frameworks, platforms or code you can fork in order to start your own DAO. It can have its own blockchain, or live as a smart contract on top of a platform like <a href='https://ethereum.org/' target='_blank'>Ethereum</a> or <a href='https://www.rsk.co/' target='_blank'>Rootstock</a>.</p></MDBCol>
+				<MDBCol className='col-12'><Link to={ROUTES.CREATE_INFO} className='btn btn-primary ml-0'>Start a DAO</Link></MDBCol>
+			</MDBRow>
+			<MDBRow className='border-top border-bottom pt-4 pb-3'>
+				<MDBCol className='col-12'><h3>Featured DAOs</h3></MDBCol>
+				<MDBCol className='col-12'>
+					<MDBRow className='flex-wrap'>
+						{daos.map(dao => (
+							<MDBCol sm='6' md='3' className='text-center'>
+		            <MDBView hover zoom>
+									<Link to={`${ROUTES.DAOS}/${dao.id}`}>
+										<img src={dao.image} className='img-thumbnail' />
+									</Link>
+		              <MDBMask className="flex-center" overlay="stylish-strong">
+		                <h4 className="white-text font-weight-bolder">{dao.title}</h4>
+		              </MDBMask>
+		            </MDBView>
+							</MDBCol>
+						))}
+					</MDBRow>
+				</MDBCol>
+			</MDBRow>
+		</MDBContainer>
 	</div>
 )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import './styles/main.scss'
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import "mdbreact/dist/css/mdb.css"
+import $ from 'jquery'
+import Popper from 'popper.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
