213670ad89bc11d7103f198c3495e106654acc89
Mauro <mauro@osn.global>
2019-07-22 14:45:22 +0200

    Add social networks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
 export default () => (
-	<div className='fixed-bottom bg-primary p-4 d-flex'>
+	<div className='fixed-bottom bg-primary p-4 d-flex justify-content-between pl-5 pr-5'>
 		<p className='text-light m-0 align-self-center'>DAO Browser by <strong><a className='text-light' href='https://twitter.com/emivelazquez6' target='_blank'>emi</a></strong> and <strong><a className='text-light' href='https://twitter.com/MauroVelazquezz' target='_blank'>mauerv</a></strong>
 		</p>
+		<div>
+			<a href='https://github.com/mauerv/OneBigLab' target='__blank' className='mr-3 ml-3'>
+				<i className="fab fa-github fa-2x light-icon" aria-hidden="true"></i>
+			</a>
+			<a href='https://twitter.com/daobrowser' target='__blank' className='mr-3 ml-3'>
+				<i className="fab fa-twitter fa-2x light-icon" aria-hidden="true"></i>
+			</a>
+			<a href='https://t.me/joinchat/Mrz4fVjr60sJ8xNImnC6xw' target='__blank' className='mr-3 ml-3'>
+				<i className="fab fa-telegram fa-2x light-icon" aria-hidden="true"></i>
+			</a>
+			<a href='https://medium.com/dao-browser' target='__blank' className='mr-3 ml-3'>
+				<i className="fab fa-medium fa-2x light-icon" aria-hidden="true"></i>
+			</a>
+			<a href='https://www.reddit.com/r/daobrowser' target='__blank' className='mr-3 ml-3'>
+				<i className="fab fa-reddit fa-2x light-icon" aria-hidden="true"></i>
+			</a>
+		</div>
 	</div>
 )
