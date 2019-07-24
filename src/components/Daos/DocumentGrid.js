f702dd026229b8469f60bc7aca7ebdd16c4ae468
Mauro <mauro@osn.global>
2019-07-24 11:17:39 +0200

    Add conditional rendering to Important Docs section

--- src/components/Daos/DocumentGrid.js
+++ src/components/Daos/DocumentGrid.js
@@ -4,14 +4,20 @@ export default ({ documents }) => (
   <div>
     <h3>Important Docs</h3>
     <div className='row'>
-      {documents.map(document => (
-        <div className='col-2 text-center'>
-          <a href={document.url}>
-            <i className="far fa-file-alt fa-4x secondary-icon"></i>
-            <p className='mt-1'>{document.title}</p>
-          </a>
+      {documents.length !== 0 ? (
+        documents.map(document => (
+          <div className='col-2 text-center'>
+            <a href={document.url}>
+              <i className="far fa-file-alt fa-4x secondary-icon"></i>
+              <p className='mt-1'>{document.title}</p>
+            </a>
+          </div>
+        ))
+      ) : (
+        <div className='col-12'>
+          <p>There are no available docs.</p>
         </div>
-      ))}
+      )}
     </div>
   </div>
 )
